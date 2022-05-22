344496b041c639c46ef5c030f5f2af73875095ae
Dhansy <avatarich.wafdhansyah@gmail.com>
2022-05-22 07:49:23 +0700

    Modul Polimorphism

---
+++ Polymorphism/src/polymorphism/Staff.java
@@ -0,0 +1,16 @@
+
+package polymorphism;
+
+public class Staff extends Pegawai {
+    private static final int gajiStaf=50000;
+    private static final int bonusStaf=10000;
+    
+    @Override
+    public int gaji(){
+        return gajiStaf;
+    }
+    public int Bonus(){
+        return bonusStaf;
+    }
+
+}
